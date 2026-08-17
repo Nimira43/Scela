b47737922a6f639d03f7d5f789414874b414b31c
Nimira43 <n.rabone72@aol.com>
2026-08-17 11:00:17 +0100

    Continue work on UserMovies.

--- src/app/(private)/user/movies/page.tsx
+++ src/app/(private)/user/movies/page.tsx
@@ -1,4 +1,24 @@
+import { getActiveMovies } from '@/actions/movies'
+import InfoMessage from '@/components/functional/info-message'
+import { IMovie } from '@/interfaces'
+
 async function UserMovies() {
+  const response: any = await getActiveMovies({})
+
+  if (!response.success) {
+    return (
+      <InfoMessage message='Failed to load movies.' />
+    )
+  }
+  
+  if (response.data.length === 0) {
+    return (
+      <InfoMessage message='No movies found.' />
+    )
+  }
+
+  const movie: IMovie[] = response.data
+
   return (
     <div>
       User Movies
